c01f2b9c33fb4dfe8ad3d4eba60d27ac189c3158
Rafiul-Islam <rafi.cse.ru@gmail.com>
2025-01-22 00:20:55 +0600

    feat: Style the active link in teh navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,19 +1,30 @@
+"use client";
+
 import React from 'react';
 import Link from "next/link";
 import {AiFillBug} from "react-icons/ai";
+import {usePathname} from "next/navigation";
+import classNames from "classnames";
+
+const links = [
+    {label: 'Dashboard', url: '/'},
+    {label: 'Issues', url: '/issues'},
+];
 
 const Navbar = () => {
-    const links = [
-        {label: 'Dashboard', url: '/'},
-        {label: 'Issues', url: '/issues'},
-    ];
+    const pathname = usePathname();
 
     return (
         <nav className='flex justify-between items-center space-x-6 h-20 border-b shadow mb-5 px-10'>
             <Link href={'/'}><AiFillBug size={20}/></Link>
             <ul className='flex gap-10'>
                 {links.map((link) => (
-                    <li key={link.url} className='text-zinc-500 hover:text-zinc-800 transition-colors duration-150'>
+                    <li key={link.url}
+                        className={classNames({
+                            'text-zinc-900': pathname === link.url,
+                            'text-zinc-500': true,
+                            'hover:text-zinc-800 transition-colors duration-150': true,
+                        })}>
                         <Link href={link.url}>{link.label}</Link>
                     </li>
                 ))}
