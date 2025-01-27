e85ce808f6a6bc55c8cb375237e7e6ade4d9a36d
Rafiul-Islam <rafi.cse.ru@gmail.com>
2025-01-27 23:34:43 +0600

    refactor: Navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -4,83 +4,83 @@ import Link from "next/link";
 import {AiFillBug} from "react-icons/ai";
 import {usePathname} from "next/navigation";
 import classNames from "classnames";
-import {Avatar, Box, Button, Container, DropdownMenu, Flex, Text} from "@radix-ui/themes";
+import {Avatar, Box, Container, DropdownMenu, Flex, Text} from "@radix-ui/themes";
 import {useSession} from "next-auth/react";
 
 const links = [
-    {label: <AiFillBug size={20}/>, url: '/'},
-    {label: 'Dashboard', url: '/dashboard'},
+    {label: 'Dashboard', url: '/'},
     {label: 'Issues', url: '/issues/list'},
 ];
 
-const authLinks = [
-    {label: 'Login', url: '/api/auth/signin'},
-    {label: 'Logout', url: '/api/auth/signout'},
-];
-
 const Navbar = () => {
-    const pathname = usePathname();
-    const {data, status} = useSession();
-    console.log(data?.user)
-
     return (
-        <nav className='border-b shadow mb-5 px-5 py-3'>
+        <nav className='border-b shadow mb-5 px-5'>
             <Container>
-                <Flex height='100%' justify='between' align='center'>
+                <Flex className='h-16' justify='between' align='center'>
                     <Box>
-                        <ul className='flex gap-10'>
-                            {links.map((link) => (
-                                <li key={link.url}
-                                    className={classNames({
-                                        'text-zinc-900': pathname === link.url,
-                                        'text-zinc-500': true,
-                                        'hover:text-zinc-800 transition-colors duration-150': true,
-                                    })}>
-                                    <Link href={link.url}>{link.label}</Link>
-                                </li>
-                            ))}
-                        </ul>
+                        <NavLinks/>
                     </Box>
                     <Box>
-                        <ul className='flex gap-10'>
-                            {status === 'unauthenticated' &&
-                                <li className='text-zinc-500 hover:text-zinc-800 transition-colors duration-150'>
-                                    <Link href='/api/auth/signin'>Login</Link>
-                                </li>
-                            }
-                            {status === 'authenticated' &&
-                                <li className='text-zinc-500 hover:text-zinc-800 transition-colors duration-150'>
-                                    <DropdownMenu.Root>
-                                        <DropdownMenu.Trigger>
-                                            <Avatar
-                                                className='border-gray-400 border-2'
-                                                role='button'
-                                                size="3"
-                                                src={data?.user?.image!}
-                                                fallback="?"
-                                                radius='full'
-                                                referrerPolicy='no-referrer'
-                                            />
-                                        </DropdownMenu.Trigger>
-                                        <DropdownMenu.Content variant="soft">
-                                            <Flex direction='column' className='p-3'>
-                                                <Text>{data?.user?.name}</Text>
-                                                <Text size='1'>{data?.user?.email}</Text>
-                                            </Flex>
-                                            <DropdownMenu.Separator />
-                                            <DropdownMenu.Item color='red' role='button'>
-                                                <Link href='/api/auth/signout'>Logout</Link>
-                                            </DropdownMenu.Item>
-                                        </DropdownMenu.Content>
-                                    </DropdownMenu.Root>
-                                </li>
-                            }
-                        </ul>
+                        <AuthStatus/>
                     </Box>
                 </Flex>
             </Container>
         </nav>
     );
 };
+
+const NavLinks = () => {
+    const pathname = usePathname();
+    return (
+        <ul className='flex gap-10'>
+            <li>
+                <Link href='/'>
+                    <AiFillBug size={20}/>
+                </Link>
+            </li>
+            {links.map((link) => (
+                <li key={link.url}
+                    className={classNames({
+                        'nav-link': true,
+                        '!text-zinc-900': pathname === link.url,
+                    })}>
+                    <Link href={link.url}>{link.label}</Link>
+                </li>
+            ))}
+        </ul>
+    )
+}
+
+const AuthStatus = () => {
+    const {data, status} = useSession();
+
+    if (status === 'loading') return null;
+    if (status === 'unauthenticated') return <Link className='nav-link' href='/api/auth/signin'>Login</Link>
+    return (
+        <DropdownMenu.Root>
+            <DropdownMenu.Trigger>
+                <Avatar
+                    className='border-gray-400 border-2'
+                    role='button'
+                    size="3"
+                    src={data?.user?.image!}
+                    fallback="?"
+                    radius='full'
+                    referrerPolicy='no-referrer'
+                />
+            </DropdownMenu.Trigger>
+            <DropdownMenu.Content variant="soft">
+                <Flex direction='column' className='p-3'>
+                    <Text>{data?.user?.name}</Text>
+                    <Text size='1'>{data?.user?.email}</Text>
+                </Flex>
+                <DropdownMenu.Separator/>
+                <DropdownMenu.Item color='red' role='button'>
+                    <Link href='/api/auth/signout'>Logout</Link>
+                </DropdownMenu.Item>
+            </DropdownMenu.Content>
+        </DropdownMenu.Root>
+    )
+}
 
 export default Navbar;
