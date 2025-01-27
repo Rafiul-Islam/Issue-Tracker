b9b8f54cf01065878e78aacd90dbb2c9a5cc951c
Rafiul-Islam <rafi.cse.ru@gmail.com>
2025-01-27 09:54:00 +0600

    feat: Implement delete issue API

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -2,6 +2,17 @@ import {NextRequest, NextResponse} from "next/server";
 import {IssueSchema} from "@/app/validationSchema";
 import {prisma} from "@/prisma/client";
 
+export const GET = async (req: NextRequest, {params}: { params: Promise<{ id: string }> }) => {
+    const {id} = await params;
+    try {
+        const issue = await prisma.issue.findUnique({where: {id: +id}});
+        if (!issue) return NextResponse.json({success: false, message: "Issue not found"}, {status: 404});
+        return NextResponse.json({success: true, message: "Issue fetched successfully", payload: {issue}}, {status: 200});
+    } catch (err) {
+        return NextResponse.json({success: false, message: "Something went wrong", payload: {error: err}}, {status: 500});
+    }
+}
+
 export const PATCH = async (req: NextRequest, {params}: { params: Promise<{ id: string }> }) => {
     const {id} = await params;
     const body = await req.json();
@@ -37,5 +48,17 @@ export const PATCH = async (req: NextRequest, {params}: { params: Promise<{ id: 
                 payload: {error: err}
             }, {status: 500});
         }
+    }
+}
+
+export const DELETE = async (req: NextRequest, {params}: { params: Promise<{ id: string }> }) => {
+    const {id} = await params;
+    try {
+        const issue = await prisma.issue.findUnique({where: {id: +id}});
+        if (!issue) return NextResponse.json({success: false, message: "Issue not found"}, {status: 404});
+        await prisma.issue.delete({where: {id: +id}})
+        return NextResponse.json({success: true, message: "Issue deleted successfully"}, {status: 200})
+    } catch (err) {
+        return NextResponse.json({success: false, message: "Something went wrong", payload: {error: err}}, {status: 500})
     }
 }
